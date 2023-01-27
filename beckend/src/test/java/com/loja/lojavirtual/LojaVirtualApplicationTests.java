7eb94e78cc6adeb1ef3b35c481a0e0114324df3a
RodrigoMachadodaSilva <87843482+RodrigoMachadodaSilva@users.noreply.github.com>
2023-01-27 17:14:46 -0300

    Inicio do Projeto

---
+++ beckend/src/test/java/com/loja/lojavirtual/LojaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loja.lojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
