1121c7b3f5c462f35f37b16295f6297e3ebc27e0
RodrigoMachadodaSilva <87843482+RodrigoMachadodaSilva@users.noreply.github.com>
2023-02-06 19:36:27 -0300

    Criação do crud de Permissoes

--- beckend/src/main/java/com/loja/lojavirtual/entity/Pessoa.java
+++ beckend/src/main/java/com/loja/lojavirtual/entity/Pessoa.java
@@ -3,6 +3,7 @@ package com.loja.lojavirtual.entity;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -12,13 +13,16 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
 
 @Entity
 @Data
@@ -38,9 +42,17 @@ public class Pessoa {
 	@Embedded
 	private Endereco endereco;
 	
-
-	//@ManyToMany
-	//private List<Permissao> permissao;
+    @OneToMany(mappedBy = "pessoa", orphanRemoval = true, cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
+    @Setter(value = AccessLevel.NONE)
+	private List<PermissaoPessoa> permissoes;
+    
+    public void setPermissaoPessoas(List<PermissaoPessoa> pp){
+        for(PermissaoPessoa p:pp){
+            p.setPessoa(this);
+        }
+        this.permissoes = pp;
+    }
+	
 	
 	//@Temporal(TemporalType.TIMESTAMP)
 	//private Date dataCriacao;
